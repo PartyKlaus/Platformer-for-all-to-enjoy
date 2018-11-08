d6f1406f6e515b0036d89b6850b01cde7d800450
PartyKlaus <43466974+PartyKlaus@users.noreply.github.com>
2018-11-08 15:08:21 +0100

    Add files via upload

--- BigBaller/Game5/ScreenManager.cs
+++ BigBaller/Game5/ScreenManager.cs
@@ -13,9 +13,50 @@ namespace Game5
 {
     public class ScreenManager
     {
-     
 
-        
+        float bgWidth;
+        float bgHeight;
+
+        Texture2D background2;
+        Vector2[] positions;
+
+        int bgSpeed;
+
+        public void Initialize(float screenWidth, float screenHeight, Texture2D background2, int bgSpeed)
+        {
+            this.background2 = background2;
+            bgHeight = screenHeight;
+            bgWidth = background2.Width * (screenHeight / background2.Height);
+
+            this.bgSpeed = bgSpeed;
+
+            positions = new Vector2[(int)screenWidth / (int)bgWidth + 2];
+
+            for (int i = 0; i < positions.Length; i++)
+                positions[i].X = i * bgWidth;
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i].X -= bgSpeed;
+
+                if (positions[i].X <= -bgWidth) positions[i].X = (positions.Length - 1) * bgWidth;
+
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            for (int i = 0; i < positions.Length; i++)
+            {
+                Rectangle bgScreen = new Rectangle(int)positions[i].X,
+            }
+
+
+        }
 
     }
 }
