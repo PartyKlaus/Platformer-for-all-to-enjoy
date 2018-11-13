4d62a33d3ec8fb43ec65f9415ce21fb8ce27db5a
PartyKlaus <43466974+PartyKlaus@users.noreply.github.com>
2018-11-13 17:10:35 +0100

    Add files via upload

---
+++ BigBaller/Game5/Tile.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game5
+{
+    class Tile
+    {
+        protected Texture2D tileTexture;
+
+        private Rectangle rectangle;
+        public Rectangle Rectangle
+        {
+            get { return rectangle; }
+            protected set { rectangle = value; }
+        }
+
+        private static ContentManager content;
+        public static ContentManager Content
+        {
+            protected get { return content; }
+            set { content = value; }
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(tileTexture, rectangle, Color.White);
+        }
+
+        
+        
+    }
+
+    class CollisionTiles : Tile
+    {
+        public CollisionTiles(int i, Rectangle newRectangle)
+        {
+            tileTexture = Content.Load<Texture2D>("Tiles/Tile" + i);
+            this.Rectangle = newRectangle;
+        }
+    }
+}
